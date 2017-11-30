528e8c9dafd54c5c32ca99e531788e87e8f413b7
Unknown <baptiste.sanchez72@gmail.com>
2017-11-30 15:57:47 +0100

    Correcton By Smxme. html + js.
    Utilisation du POO (Orientation Objet) + Interaction entre Marques et Prodruits + Localisation des différents tableaux correspondant aux différentes marques.

--- stylejs.js
+++ stylejs.js
@@ -1,36 +1,58 @@
-function addMarq(){
-	if ($('#addM').val() != '') {
-		$('#addM').val();
-		$('#myUl').append('<li>'+$('#addM').val()+'</li>');
-		$('#addM').val('');
+var marque = [];
+var laMarque = "";
+var maPlace = -1;
+
+
+// function Produit (nomP, prix, quantite){
+// 	this.nomProduit = nom;
+// 	this.prix = prix;
+// 	this.quantite = quantite;
+// }
+
+$('#myBtn').click(ajoutMarque);
+$('#myBtnProduit').click(ajoutProduits);
+
+function init(){
+	$('.ligneMarque').click(selectionnerMarque);	
+}
+
+function selectionnerMarque(){
+	laMarque = $(this).html();
+	$('#titreMarque').html(laMarque);
+	$('#infoProduits').css("display","block");
+}
+
+function ajoutMarque(){
+	marque.push( new Marque($('#addM').val()));
+	afficherMarques();
+	init();
+}
+
+function afficherMarques(){
+	$('#myUl').html("");
+	for (var i = 0 ; i < marque.length ; i++){
+		$('#myUl').append('<li class="ligneMarque">'+marque[i].nom+'</li>');
 	}
 }
-$('#myBtn').click(addMarq);
 
-function addProduit(){
-	if ($('addP').val() & $('addPr').val() & $('addQ').val() != '') {
-		$('addP').val();
-		$('addPr').val();
-		$('addQ').val();
-		
+function ajoutProduits(){
+	for (var i = 0 ; i < marque.length ; i++){
+		if (marque[i].nom == laMarque){
+			maPlace = i;
+		}
 	}
 }
 
-
-
-/*var produit = {
-	init: function(nom, prix, quantite){
-	this.nom = '',
-	this.prix = '',
-	this.quantite: 0 ,
+function Produit (nomP,prixP,qteP){
+	this.nomProduit = nomP;
+	this.prixProduit = prixP;
+	this.qteProduit= qteP;
 }
+
+function Marque(nomM) {
+	this.nom = nomM;
+	this.produits = [];
 }
-var marque = {
-	nom:'Addidas',
-	produits:'',
-}*/
-
-
 
 
 
